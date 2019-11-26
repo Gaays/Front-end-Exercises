f026bcdc5a10ebec8cbbc1eee3334642780e105b
Gaays <shujiapeng@Outlook.com>
2019-11-26 15:42:54 +0800

    产品页面练习

---
+++ product-list/js/js.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+	let productCard = $(".product-card");
+	let productImg = $(".product-img");
+	let span = $(".product-color span");
+	// console.log(span)
+	span.click(function(){
+		$(".product-color span").removeClass("active");
+		$(this).addClass("active");
+		$(".product-img").css("background-image",$(this).attr("data-url"))
+		$("body").css("background",$(this).attr("data-color"));
+		console.log($(this).data("url"));
+	});
+
+}
