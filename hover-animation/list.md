dffae9dc271878037570d315a399ce2165db3d62
Gaays <shujiapeng@Outlook.com>
2019-12-09 17:27:40 +0800

    add hover-animation

---
+++ hover-animation/list.md
@@ -0,0 +1,20 @@
+div container
+	div card
+		div face face1
+			div content
+				img
+				h3
+		div face face2
+			div content
+				p
+				a
+				
+	div card
+		div face face1
+			div content
+				img
+				h3
+		div face face2
+			div content
+				p
+				a
---
+++ shop-bootstrap/js/js.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+	
+	//ajax获取数据
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', 'json/data.json', false);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			// console.log(xhr.responseText);
+			productData = JSON.parse(xhr.responseText);
+		}
+	
+	}
+	xhr.send(null);
+	console.log(productData);
+	
+	
+	
+	
+}
